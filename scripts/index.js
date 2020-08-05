0333be3abec6aa4a1514aa9850537114ea582cac
Nolwac <livinusanayo96@gmail.com>
2020-08-05 04:23:25 +0100

    Learn and Practice how to create and use a basic function

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,19 @@
 
-var template = ['how are you', 'hello', 'your name', 'who created you'];
-var response = ['I am fine, what of you', 'hi, how are you doing', 'My name is Jarvis, how may I help you',  'I was created by nwafor']
-
-var message;
-while(message != 'bye'){
-  message = prompt("type your message:");
-  for(var i = 0; i < template.length; i++){
-    var temp = template[i];
-    var check = message.includes(temp);
-    if(check == true){
-      alert(response[i]);
+function chatbot(){
+  var template = ['how are you', 'hello', 'your name', 'who created you'];
+  var response = ['I am fine, what of you', 'hi, how are you doing', 'My name is Jarvis, how may I help you',  'I was created by nwafor']
+  
+  var message;
+  while(message != 'bye'){
+    message = prompt("type your message:");
+    for(var i = 0; i < template.length; i++){
+      var temp = template[i];
+      var check = message.includes(temp);
+      if(check == true){
+        alert(response[i]);
+      }
     }
   }
 }
+chatbot();
+chatbot();
