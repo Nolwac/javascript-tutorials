aae63a88ec029416026d710352bf838e92b4e79a
Nolwac <livinusanayo96@gmail.com>
2020-08-07 04:43:12 +0100

    Learn and Practice array methods part 4

--- scripts/index.js
+++ scripts/index.js
@@ -26,6 +26,20 @@ function chatbot(name, creator="Google"){
 var array1 = ["prime", "livinus", "henry", "jude"];
 var array2 = ["jacob", "williams", "prince"];
 var array4 = [300, 4,1,6,20,9];
-var array3 = array1.splice(2, 2, "Victor", "julius");
-var array5 = array2.slice(0, 2);
-document.write(array5);
+var check = array1.includes("Jude");
+function inc(item){
+  if(item.includes("e")){
+    return item;
+  }else{
+    return "missing"
+  }
+}
+
+function finder(item){
+  return item.includes("n");
+}
+var check2 = array1.map(inc);
+var found  = array1.find(finder);
+var str = array2.toString();
+var str2 = array2.join("***----");
+document.write(str2);
