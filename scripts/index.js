44d481e0184bd1f5a9bddd1f11d935e2fc53bc1a
Nolwac <livinusanayo96@gmail.com>
2020-08-17 06:04:26 +0100

    Learn the new methods of variable declaration using let and const

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,9 @@
 
-console.log(Math);
-var num1 = 56.7;
-var num2 = 57.001;
-//document.write(Math.random());
-var num3 = -58;
-var num4 = 78;
-var num5 = 1e-23;
-var num6 = 1e3;
-//document.write(Math.E);
+function writer(){
+  if(true){
+    let name = "Livinus Anayo";
+  }
+  return name;
+}
 
-setInterval(function(){
-  var dice = Math.ceil(Math.random()*6);
-  document.write(dice)
-}, 1000);
+document.write(writer());
