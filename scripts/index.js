e19e8c9c8a4c44105d5abb0d84dc1a2bcc496975
Nolwac <livinusanayo96@gmail.com>
2020-08-17 05:36:10 +0100

    Learn and Practice how to use the Math object

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,15 @@
 
-var upperDiv = document.getElementById('upper');
-var child = document.getElementById('child');
+console.log(Math);
+var num1 = 56.7;
+var num2 = 57.001;
+//document.write(Math.random());
+var num3 = -58;
+var num4 = 78;
+var num5 = 1e-23;
+var num6 = 1e3;
+//document.write(Math.E);
 
-child.addEventListener('click', function(e){
-  console.log('this is a child h2 element');
-}, true);
-
-upperDiv.addEventListener('click', function(e){
-  console.log('this is the child parent');
-}, true);
+setInterval(function(){
+  var dice = Math.ceil(Math.random()*6);
+  document.write(dice)
+}, 1000);
