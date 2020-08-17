d8821101a2383acfd121dc4fba7ac85ca9a8a741
Nolwac <livinusanayo96@gmail.com>
2020-08-17 04:55:15 +0100

    Practice using the properties of the event object and learn event bubbling and capturing

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,11 @@
 
-var div = document.getElementById('upper');
-div.style.width = "50px";
-div.style.height = "50px";
+var upperDiv = document.getElementById('upper');
+var child = document.getElementById('child');
 
-function changeColor(elem){
-  elem.style.backgroundColor = 'red';
-}
+child.addEventListener('click', function(e){
+  console.log('this is a child h2 element');
+}, true);
 
-div.onclick = function(e){
-  div.style.backgroundColor = 'orange';
-}
-
-function generator(){
-  var elem = document.createElement('div');
-  elem.style.width = "50px";
-  elem.style.height = "50px";
-  elem.style.position = 'absolute';
-  var left = Math.random()*window.innerWidth;
-  var top = Math.random()*window.innerHeight;
-  
-  elem.style.left = left+"px";
-  elem.style.top = top+"px";
-  elem.style.backgroundColor = 'crimson';
-  document.body.appendChild(elem);
-  function colorChanger(){
-    elem.style.backgroundColor = 'orange';
-  }
-  function widthChanger(){
-    elem.style.width = '100px';
-  }
-  elem.addEventListener('mouseover', widthChanger);
-  elem.addEventListener('mouseover', colorChanger);
-  elem.removeEventListener('mouseover', colorChanger);
-  elem.removeEventListener('mouseover', widthChanger);
-}
-generator();
-setInterval(function(){generator();}, 2000);
-//document.addEventListener('DOMContentLoaded', function(){document.write("loaded")})
-window.onload = function(){
-  document.write('window is loaded');
-}
+upperDiv.addEventListener('click', function(e){
+  console.log('this is the child parent');
+}, true);
