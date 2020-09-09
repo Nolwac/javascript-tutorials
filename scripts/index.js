c21362bab826996f2f5cb451c16aab2345d5d934
Nolwac <livinusanayo96@gmail.com>
2020-09-09 16:57:31 +0100

    Learn and practice how to create classes in javascript and to create static methods

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,18 @@
 
-function writers(surname, lastname, ...names){
-  console.log(names)
-  document.write(names)
+class Employee{
+  constructor(name, salary){
+    this.name = name;
+    this.salary = salary;
+  }
+  getPaid(){
+    document.write(this.name + " has been paid " + this.salary)
+  }
+  static saySomething(message){
+    document.write(message);
+  }
 }
 
-writers("Chinua Achebe", "Soyinka", "Chimammanda");
+let employ1 = new Employee("Nwafor Livinus", "200,000");
+employ1.getPaid();
+Employee.saySomething("This will give an error");
+console.log(employ1);
