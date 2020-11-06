01c10902a5cbee5bc04333299a3f082603eb2ea9
Nolwac <livinusanayo96@gmail.com>
2020-11-06 11:34:51 +0100

    Learn and pracitce object destructuring

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,27 @@
-let array = [
-  ["Jane", "Prince"],
-  [1, 2, 3]
-  ];
+let person = {
+  name:"Livinus",
+  country:"Nigeria",
+  state:"Ebonyi",
+  interest:"Programming technology",
+  other:{
+    friends:["Prince", "Julius", "Michael"],
+    height:2.15,
+  }
+}
+let firstname, interest, country, state, occupation;
 
-let [[name1], [age, height]]=array;
-
-document.write(name1)
-document.write(age);
-document.write(height);
+({name:firstname="Nwafor", interest, country, state, occupation="Tailoring"} = {
+  name:"Livinus",
+  country:"Nigeria",
+  state:"Ebonyi",
+  interest:"Programming technology",
+  other:{
+    friends:["Prince", "Julius", "Michael"],
+    height:2.15,
+  }
+})
+//let {other:{height:h}, interest} = person;
+// document.write(firstname);
+// document.write(occupation);
+//document.write(h)
+document.write(interest)
