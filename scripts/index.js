3dc71e59cc88bbde4adcc245c1449789b9976152
Nolwac <livinusanayo96@gmail.com>
2020-11-06 11:57:42 +0100

    Learn and practice how to use template string syntax

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,3 @@
-let person = {
-  name:"Livinus",
-  country:"Nigeria",
-  state:"Ebonyi",
-  interest:"Programming technology",
-  other:{
-    friends:["Prince", "Julius", "Michael"],
-    height:2.15,
-  }
-}
-let firstname, interest, country, state, occupation;
-
-({name:firstname="Nwafor", interest, country, state, occupation="Tailoring"} = {
-  name:"Livinus",
-  country:"Nigeria",
-  state:"Ebonyi",
-  interest:"Programming technology",
-  other:{
-    friends:["Prince", "Julius", "Michael"],
-    height:2.15,
-  }
-})
-//let {other:{height:h}, interest} = person;
-// document.write(firstname);
-// document.write(occupation);
-//document.write(h)
-document.write(interest)
+let name = "Nwafor Livinus";
+let height = 9.5;
+document.write(`my name is ${name} and my height is ${height}, don't touch me ${Math.abs(4-20)}`);
