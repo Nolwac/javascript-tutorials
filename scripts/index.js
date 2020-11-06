09d56b3de7ae28fd6d54cc18dedffdea707267ae
Nolwac <livinusanayo96@gmail.com>
2020-11-06 10:02:03 +0100

    Learn and Practice spread operation in Javascript

--- scripts/index.js
+++ scripts/index.js
@@ -1,47 +1,11 @@
+let names = ['prince', 'charles', 'nwafor', 'sandra'];
+let otherNames = ['solomon', 'henry', 'williams'];
 
-function Person(name, age){
-  this.name = name;
-  this.age = age;
-  this.hello = function(){
-    document.write("This is a prove of inheritance");
-  }
-}
+let fullList = [...names, ...otherNames];
+let info = {name:"Nwafor", age:20, height:2.3}
+let otherInfo = {firstName:"Livinus", country:"Nigeria"}
 
-class Employee extends Person{
-  constructor(name, age, salary){
-    super(name, age)
-    this.salary = salary;
-  }
-  getPaid(){
-    document.write(this.name + " has been paid " + this.salary)
-  }
-  static saySomething(message){
-    document.write(message);
-  }
-  get fullInfo(){
-    return this.name + " " + this.salary
-  }
-  set setName(name){
-    this.name = name;
-  }
-  set setSalary(salary){
-    this.salary = salary;
-  }
-}
+let fullInfo = {...info, ...otherInfo}
 
-class Executive extends Employee{
-  constructor(name, age, salary, role){
-    super(name, age, salary);
-    this.position = role;
-  }
-  playRole(){
-    document.write(this.name + " the " + this.position + " is now play his role");
-  }
-}
-
-let employ1 = new Employee("Nwafor Livinus", 30, 200000);
-let ceo = new Executive("Onah Sochima", 30, 1000000, "Chief Executive Officer");
-employ1.setName = "Nwafor Anayo";
-employ1.setSalary = 5000000;
-document.write(employ1.fullInfo);
-console.log(employ1);
+console.log(fullInfo);
+console.log(fullList);
