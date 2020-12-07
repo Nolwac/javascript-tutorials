d389e50e4fccf945896e876e9071e6caef3337ce
Nolwac <livinusanayo96@gmail.com>
2020-12-07 08:29:05 +0100

    Practice how to work with the window.navigator object

--- scripts/index.js
+++ scripts/index.js
@@ -1,50 +1,24 @@
-console.log(window.history);
-
+console.log(window.navigator)
 let but = document.createElement('button');
-but.innerHTML = "Change URL";
-document.body.appendChild(but)
+but.innerHTML =  "copy";
+document.body.appendChild(but);
+
+but.onclick = function(e){
+  // window.navigator.geolocation.getCurrentPosition(function(obj){
+  //   console.log(obj)
+  // },
+  // function(obj){
+  //   console.log(obj);
+  //   console.log("attempt failed")
+  // });
+  //window.navigator.vibrate([500, 150, 500, 150, 500])
+  window.navigator.clipboard.writeText("Hello world");
+}
 
 let but2 = document.createElement('button');
-but2.innerHTML = "Back";
+but2.innerHTML =  "paste";
 document.body.appendChild(but2);
 
-let but3 = document.createElement('button');
-but3.innerHTML = "Forward";
-document.body.appendChild(but3);
-
-let but4 = document.createElement('button');
-but4.innerHTML = "Go forward by 1";
-document.body.appendChild(but4);
-
-let but5 = document.createElement('button');
-but5.innerHTML = "go backward by 2";
-document.body.appendChild(but5);
-
-let but6 = document.createElement('button');
-but6.innerHTML = "Change history state";
-document.body.appendChild(but6);
-let num = 0;
-but.onclick = function(){
-  window.history.pushState({}, "History", "/test/history/"+num);
-  num++;
-}
-
 but2.onclick = function(){
-  window.history.back();
-}
-
-but3.onclick = function(){
-  window.history.forward();
-}
-
-but4.onclick = function(){
-  window.history.go(1);
-}
-
-but5.onclick = function(){
-  window.history.go(-2);
-}
-
-but6.onclick = function(){
-  window.history.replaceState({id:10}, "Hello State", "/test/history/10-changed")
+  console.log(window.navigator.clipboard.readText())
 }
