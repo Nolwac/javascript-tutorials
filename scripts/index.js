346dab4847647facf4f4518f12d2562526569253
Nolwac <livinusanayo96@gmail.com>
2020-12-29 05:32:52 +0100

    Learn and practice how to work with winodow.localStorage

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,5 @@
-//console.log(document.cookie);
-let expire = new Date();
-document.cookie = "height=2.3; expires="+expire.toUTCString();
-document.cookie = "country=Nigeria";
-document.cookie = "state=Ebonyi";
-
-function getData(key){
-  let cookieString = document.cookie;
-  let keyValuePairs = cookieString.split("; ");//["username=Nwafor", "height=7.2"]
-  for(i=0; i<keyValuePairs.length; i++){
-    let pair = keyValuePairs[i];
-    let [pairKey, pairValue] = pair.split("=");//["username", "nwafor"]
-    if(pairKey==key){
-      return pairValue;
-    }
-  }
-  return null;
-}
-console.log(getData("country"))
-console.log(document.cookie)
+console.log(localStorage)
+localStorage.setItem("name", "Nwafor Livinus");
+//window.localStorage.clear()
+console.log(window.localStorage.getItem("name"));
+console.log(window.localStorage.getItem("age"));
